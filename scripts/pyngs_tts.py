3e3871f09f0a88e1b7b38bffcef227c5641493b1
Patrick LOmbard <pdl30@cam.ac.uk>
2015-01-20 12:33:47 +0000

    added tts

---
+++ scripts/pyngs_tts.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+########################################################################
+# 28 Oct 2014
+# Patrick Lombard, Centre for Stem Stem Research
+# Core Bioinformatics Group
+# University of Cambridge
+# All right reserved.
+########################################################################
+
+
+import pyngsplot.tts_plot
+pyngsplot.tts_plot.main()
